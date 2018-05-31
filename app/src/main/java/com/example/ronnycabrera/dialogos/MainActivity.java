68874ddac09a40b9c09027735e63266bb02b9fe6
Ronny Cabrera <ronny-38@hotmail.com>
2018-05-31 00:49:51 -0500

    Creacion de Dialogo MultiChoose

--- app/src/main/java/com/example/ronnycabrera/dialogos/MainActivity.java
+++ app/src/main/java/com/example/ronnycabrera/dialogos/MainActivity.java
@@ -7,6 +7,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -16,7 +18,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void mostrar(View v) {
-        dialogoSingleChoose();
+        dialogoMultiChoose();
     }
 
     public void dialogoAlerta() {
@@ -110,4 +112,49 @@ public class MainActivity extends AppCompatActivity {
         alertaDialogo.show();
     }
 
+    public void dialogoMultiChoose() {
+        final CharSequence[] items = {"Articulo 1","Articulo 2","Articulo 3", "Articulo 4"};
+        final ArrayList seleccinados = new ArrayList();
+
+        AlertDialog.Builder alertaDialogo = new AlertDialog.Builder(this);
+        alertaDialogo.setTitle("Mensaje : Titulo");
+
+        alertaDialogo.setPositiveButton("SI", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int i) {
+                String mensaje = "";
+                for (int j = 0; j < seleccinados.size(); j++) {
+                    mensaje = mensaje + items[Integer.parseInt(seleccinados.get(j).toString())].toString();
+                }
+                Toast.makeText(getApplicationContext(), mensaje, Toast.LENGTH_LONG).show();
+            }
+        });
+        alertaDialogo.setNegativeButton("NO", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int i) {
+                Toast.makeText(getApplicationContext(), "NO", Toast.LENGTH_LONG).show();
+            }
+        });
+        alertaDialogo.setNeutralButton("Cancelar", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int i) {
+                Toast.makeText(getApplicationContext(), "Cancelar", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        alertaDialogo.setMultiChoiceItems(items, null, new DialogInterface.OnMultiChoiceClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int i, boolean isChecked) {
+                if(isChecked) {
+                    seleccinados.add(i);
+                }else {
+                    seleccinados.remove(i);
+                }
+            }
+        });
+
+        alertaDialogo.setCancelable(true);
+        alertaDialogo.create();
+        alertaDialogo.show();
+    }
 }
