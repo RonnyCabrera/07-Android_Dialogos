20973f4fa895d4e12eca36d89dee256743e6128d
Ronny Cabrera <ronny-38@hotmail.com>
2018-05-31 00:48:22 -0500

    Creacion de Dialogo Lista

--- app/src/main/java/com/example/ronnycabrera/dialogos/MainActivity.java
+++ app/src/main/java/com/example/ronnycabrera/dialogos/MainActivity.java
@@ -16,7 +16,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void mostrar(View v) {
-        dialogoAlerta();
+        dialogoLista();
     }
 
     public void dialogoAlerta() {
@@ -40,6 +40,31 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(DialogInterface dialog, int i) {
                 Toast.makeText(getApplicationContext(), "Cancelar", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        alertaDialogo.setCancelable(true);
+        alertaDialogo.create();
+        alertaDialogo.show();
+    }
+
+    public void dialogoLista() {
+        final CharSequence[] items = {"Articulo 1","Articulo 2","Articulo 3", "Articulo 4"};
+
+        AlertDialog.Builder alertaDialogo = new AlertDialog.Builder(this);
+        alertaDialogo.setTitle("Mensaje : Titulo");
+
+        alertaDialogo.setNeutralButton("Cancelar", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(getApplicationContext(), "Cancelar", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        alertaDialogo.setItems(items, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int j) {
+                Toast.makeText(getApplicationContext(), items[j], Toast.LENGTH_LONG).show();
             }
         });
 
